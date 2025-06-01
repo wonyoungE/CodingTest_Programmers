36d05364c8081197ae9f0cc7862312f952353e26
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-06-01 17:06:22 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.18 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            answer++;
+            
+            while(true) {
+                if(answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                    answer++;
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
