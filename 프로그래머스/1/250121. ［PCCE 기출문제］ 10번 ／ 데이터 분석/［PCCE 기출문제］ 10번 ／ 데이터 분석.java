3868d27d12d541e4ffdf11c529b5eaf5085ab8bb
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-06-15 16:40:40 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 5.29 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,21 +1,15 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Collections;
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        HashMap<String, Integer> map = new HashMap<>();
+        List<int[]> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         map.put("code", 0);
         map.put("date", 1);
         map.put("maximum", 2);
         map.put("remain", 3);
         
-        int e = map.get(ext);
-        int s = map.get(sort_by);
-        
-        ArrayList<int[]> list = new ArrayList<>();
+        int e = map.get(ext), s = map.get(sort_by);
         
         for(int[] d : data) {
             if(d[e] < val_ext) {
@@ -23,13 +17,24 @@ class Solution {
             }
         }
         
-        Collections.sort(list, (o1, o2) -> o1[s] - o2[s]);
-        
+        // 정렬
         int[][] answer = new int[list.size()][];
         
-        for(int i = 0; i < answer.length; i++) {
+        for(int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
+        
+        
+        int[] tmp = new int[data[0].length];
+        for(int i = 0; i < answer.length - 1; i++) {
+            for(int j = i; j < answer.length; j++) {
+                if(answer[i][s] > answer[j][s]) {
+                    tmp = answer[i];
+                    answer[i] = answer[j];
+                    answer[j] = tmp;
+                }
+            }
+        }        
         
         return answer;
     }
