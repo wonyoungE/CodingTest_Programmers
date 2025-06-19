2cb0f78a974f49c136525c013c7a3496113f2249
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-06-20 08:42:29 +0900

    [level 1] Title: 소수 찾기, Time: 28.21 ms, Memory: 55.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        for(int i = 0; i <= n; i++) {
+            if(isPrime[i]) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
