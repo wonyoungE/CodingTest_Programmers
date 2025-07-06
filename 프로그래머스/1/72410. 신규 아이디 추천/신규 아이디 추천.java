dae5a6700b2e13cd1b5a99d232c427f6198dfb5b
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-07-06 15:36:19 +0900

    [level 1] Title: 신규 아이디 추천, Time: 11.81 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,20 +3,21 @@ class Solution {
         // 1단계
         new_id = new_id.toLowerCase();
         // 2단계
-        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+        new_id = new_id.replaceAll("[^a-z0-9\\-\\_\\.]", "");
         // 3단계
-        new_id = new_id.replaceAll("[.]{2,}", ".");
+        while(new_id.contains("..")) {
+            new_id = new_id.replace("..", ".");
+        }
+        // new_id = new_id.replaceAll("[\\.]{2,}", ".");
         // 4단계
-        new_id = new_id.replaceAll("^[.]|[.]$", "");
+        new_id = new_id.replaceAll("^[\\.]|[\\.]$", "");
         // 5단계
         new_id = new_id.length() == 0 ? "a" : new_id;
         // 6단계
-        new_id = new_id.length() >= 16 ? new_id.substring(0, 15).replaceAll("[.]$", "") : new_id;
+        new_id = new_id.length() >= 16 ? new_id.substring(0, 15).replaceAll("[\\.]$", "") : new_id;
         // 7단계
-        if(new_id.length() <= 2) {
-            while(new_id.length() < 3) {
-                new_id += new_id.charAt(new_id.length()-1);
-            }
+        while(new_id.length() < 3) {
+            new_id += new_id.charAt(new_id.length()-1);
         }
         
         return new_id;
