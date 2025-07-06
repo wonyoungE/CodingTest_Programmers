07c80b987fa11b9a0ec49db27bcdce47357cb9a7
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-07-06 12:36:10 +0900

    [level 1] Title: 실패율, Time: 79.89 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,31 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int N, int[] stages) {        
-        int[] totalCnt = new int[N+1]; // 스테이지에 도달한 플레이어 수
-        int[] currentCnt = new int[N+1];  // 클리어하지 못하고 있는 플레이어 수
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        // N+1 -> 1번부터 N번 스테이지, 모두 클리어 시 N+1
+        // 스테이지 도달한 사람 -> 0번부터 stages[i]-1번까지++
+        // ex) stages[i] = 3이면 0, 1, 2 모두 ++
+        int[] current = new int[N+1];
+        // 스테이지 클리어 못하고 머무르는 사람
+        int[] uncleared = new int[N+1];
         
         for(int i = 0; i < stages.length; i++) {
             for(int j = 0; j < stages[i]; j++) {
-                totalCnt[j]++;
+                current[j]++;
             }
-            currentCnt[stages[i] - 1]++;
+            // ex) stages[i] = 2이면 0부터 시작이니까 1번 인덱스
+            uncleared[stages[i]-1]++;
         }
         
-        // 스테이지 번호, 실패율
-        Map<Integer, Double> map = new HashMap<>();
+        Map<Integer, Double> failedRate = new HashMap<>();
         for(int i = 0; i < N; i++) {
-            if(totalCnt[i] == 0 || currentCnt[i] == 0) {
-                map.put(i+1, 0.0);
+            if(current[i] == 0 || uncleared[i] == 0) {
+                failedRate.put(i+1, 0.0);
             } else {
-                map.put(i+1, (double)currentCnt[i] / (double)totalCnt[i]);
+                failedRate.put(i+1, (double)uncleared[i] / (double)current[i]);
             }
         }
         
-        List<Integer> list = new ArrayList<>(map.keySet());
+        List<Integer> keySet = new ArrayList<>(failedRate.keySet());
         
-        list.sort((o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
+        keySet.sort((o1, o2) -> failedRate.get(o2).compareTo(failedRate.get(o1)));
         
-        return list.stream().mapToInt(i->i).toArray();
+        for(int i = 0; i < N; i++) {
+            answer[i] = keySet.get(i);
+        }
+        
+        return answer;
     }
 }
