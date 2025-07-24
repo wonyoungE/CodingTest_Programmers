41a869c6da7e5a0e107cb8da79933c93e16ee1ee
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-07-25 08:41:36 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,7 +2,8 @@ class Solution {
     public String solution(int a, int b) {
         String answer = "";
         int[] daysOfMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] dayOfWeek = new String[]{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        // 2016년 시작 요일 -> 금요일
+        String[] dayOfWeek = new String[]{"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         
         int sum = 0;
         for(int i = 0; i < a-1; i++) {
@@ -11,8 +12,7 @@ class Solution {
         // 1월1일과 a월b일의 일 수 차이
         sum += (b - 1);
         
-        // 1월 1일 금요일 -> 5번 인덱스
-        answer = dayOfWeek[(sum+5) % 7];
+        answer = dayOfWeek[sum % 7];
     
         return answer;
     }
