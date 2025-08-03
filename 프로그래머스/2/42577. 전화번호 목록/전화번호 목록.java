a958b705ee81e2e3068dcb3348a1030a8b3b36f2
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-03 13:08:21 +0900

    [level 2] Title: 전화번호 목록, Time: 383.34 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,14 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
-        for(int i = 0; i < phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
+        for(int i = 1; i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) {
                 return false;
             }
         }
-        
         return true;
     }
 }
