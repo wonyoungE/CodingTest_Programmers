ee02b9af696fbaa24fb13fd353ff01100440028e
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-03 13:24:39 +0900

    [level 2] Title: 전화번호 목록, Time: 312.32 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,27 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+//         Arrays.sort(phone_book);
         
-        for(int i = 1; i < phone_book.length; i++) {
-            if(phone_book[i].startsWith(phone_book[i-1])) {
-                return false;
+//         for(int i = 1; i < phone_book.length; i++) {
+//             if(phone_book[i].startsWith(phone_book[i-1])) {
+//                 return false;
+//             }
+//         }
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(String phone : phone_book) {
+            map.put(phone, 0);
+        }
+        
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book[i].length(); j++) {
+                if(map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
             }
         }
+        
         return true;
     }
 }
