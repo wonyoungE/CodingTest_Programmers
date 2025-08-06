b5bc6d30dc113a4c6baaf04956c748e088464280
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-07 01:22:58 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int x_min = 50, y_min = 50, x_max = 0, y_max = 0;
+        int min_x = Integer.MAX_VALUE;
+        int min_y = Integer.MAX_VALUE;
+        int max_x = Integer.MIN_VALUE;
+        int max_y = Integer.MIN_VALUE;
         
         for(int i = 0; i < wallpaper.length; i++) {
-            for(int j = 0; j < wallpaper[i].length(); j++) {
-                char file = wallpaper[i].charAt(j);
-                if(file == '#') {
-                    if(x_min > i) x_min = i;
-                    if(y_min > j) y_min = j;
-                    if(x_max < i) x_max = i;
-                    if(y_max < j) y_max = j;
+            for(int j = 0; j < wallpaper[0].length(); j++) {
+                if(wallpaper[i].charAt(j) == '#') {
+                    min_x = Math.min(min_x, i);
+                    min_y = Math.min(min_y, j);
+                    max_x = Math.max(max_x, i);
+                    max_y = Math.max(max_y, j);
                 }
             }
         }
-        return new int[]{x_min, y_min, x_max+1, y_max+1};
+        
+        return new int[] {min_x, min_y, max_x+1, max_y+1};
     }
 }
