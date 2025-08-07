a6ce201065da655fa45be7039ac207102cb8ad18
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-07 19:26:47 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         int idx1 = 0, idx2 = 0;
-
-        for (String word : goal) {
-            if (idx1 < cards1.length && word.equals(cards1[idx1])) {
+        
+        for(int i = 0; i < goal.length; i++) {
+            if(idx1 < cards1.length && cards1[idx1].equals(goal[i])) {
                 idx1++;
-            } else if (idx2 < cards2.length && word.equals(cards2[idx2])) {
+            } else if(idx2 < cards2.length && cards2[idx2].equals(goal[i])) {
                 idx2++;
             } else {
                 return "No";
