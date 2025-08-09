d095fde64940f68c67834c85fce52405b64d5987
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-09 10:53:59 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,22 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
         
-        for(int i = 0; i < targets.length; i++) {
-            String target = targets[i];
-            int sum = 0;
-            for(int j = 0; j < target.length(); j++) {
-                int min = 10000;
-                char c = target.charAt(j);
-                for(int k = 0; k < keymap.length; k++) {
-                    String key = keymap[k];
-                    if(key.indexOf(c) != -1) {  
-                        min = key.indexOf(c) + 1 < min ? key.indexOf(c) + 1 : min;
-                    }
+        Map<Character, Integer> map = new HashMap<>();
+        for(String key : keymap) {
+            for(int i = 0; i < key.length(); i++) {
+                char c = key.charAt(i);
+                
+                // map에 없는 경우, map에 저장된 눌러야하는 최소값이 i+1보다 큰 경우
+                if(map.getOrDefault(c, 0) == 0 || map.get(c) > i + 1) {
+                    map.put(c, i+1);
                 }
-                sum += min;
             }
-            answer[i] = sum >= 10000 ? -1 : sum;
+        }
+        
+        for(int i = 0; i < targets.length; i++) {
+            int cnt = 0;
+            String target = targets[i];
+            boolean isAvailable = true;
+            for(int j = 0; j < target.length(); j++) {
+                char c = target.charAt(j);
+                if(map.getOrDefault(c, 0) == 0) {
+                    answer[i] = -1;
+                    isAvailable = false;
+                    break;
+                } else {
+                    cnt += map.get(c);
+                }
+            }
+            if(isAvailable) answer[i] = cnt;
+            else answer[i] = -1;
         }
         
         return answer;
