c64e29cdcf2f89b2f8f0444caa0d208a236b7027
wonyoungE <129331945+wonyoungE@users.noreply.github.com>
2025-08-09 11:26:45 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,22 +1,11 @@
 class Solution {
     public int solution(int n) {
-        // 이진 변환 메서드 -> Integer.toString(숫자, 진법)
-        String bin = Integer.toString(n, 2);
-        int n_cnt = 0;
-        for(int i = 0; i < bin.length(); i++) {
-            char c = bin.charAt(i);
-            if(c == '1') n_cnt++;
-        }
+        int cnt = Integer.bitCount(n);
         
         while(true) {
             n++;
-            String next = Integer.toString(n, 2);
-            int next_cnt = 0;
-            for(int i = 0; i < next.length(); i++) {
-                char c = next.charAt(i);
-                if(c == '1') next_cnt++;
-            }
-            if(next_cnt == n_cnt) return n;
+            int next_cnt = Integer.bitCount(n);
+            if(next_cnt == cnt) return n;
         }
     }
 }
