d5710bb29921420b6e8e62381c3a15888b0bc374
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-08-12 09:09:52 +0900

    [level 2] Title: 구명보트, Time: 10.94 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right) {
+	        if(people[left] + people[right] > limit) right --;
+	        else {
+		        left++;
+		        right--;
+	        }
+	        // 어떠한 경우든
+	        answer++;
+        }
+        
+        return answer;
+    }
+}
