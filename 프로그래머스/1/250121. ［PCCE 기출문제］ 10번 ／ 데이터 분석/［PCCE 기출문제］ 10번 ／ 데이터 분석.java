93359b7d8b200b038fea84a9e9c94adb72c3bac1
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-08-16 22:16:15 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.44 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -3,38 +3,27 @@ import java.util.*;
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         List<int[]> list = new ArrayList<>();
+        
         Map<String, Integer> map = new HashMap<>();
         map.put("code", 0);
         map.put("date", 1);
         map.put("maximum", 2);
         map.put("remain", 3);
         
-        int e = map.get(ext), s = map.get(sort_by);
+        int e = map.get(ext);
+        int s = map.get(sort_by);
         
         for(int[] d : data) {
-            if(d[e] < val_ext) {
-                list.add(d);
-            }
+            if(d[e] <= val_ext) list.add(d);
         }
         
-        // 정렬
         int[][] answer = new int[list.size()][];
         
-        for(int i = 0; i < list.size(); i++) {
+        Collections.sort(list, (o1, o2) -> o1[s] - o2[s]);
+        
+        for(int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
-        
-        
-        int[] tmp = new int[data[0].length];
-        for(int i = 0; i < answer.length - 1; i++) {
-            for(int j = i; j < answer.length; j++) {
-                if(answer[i][s] > answer[j][s]) {
-                    tmp = answer[i];
-                    answer[i] = answer[j];
-                    answer[j] = tmp;
-                }
-            }
-        }        
         
         return answer;
     }
