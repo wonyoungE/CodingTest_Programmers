e88c15b7d2689469cc351e691b58b057a5b21269
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-11-13 08:08:34 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
 
 ### 성능 요약
 
-메모리: 82.8 MB, 시간: 0.19 ms
+메모리: 73.3 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 10일 09:04:14
+2025년 11월 13일 08:08:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,20 +1,26 @@
 class Solution {
     boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
         
+        // A->B, B->C 면 A->C 연결
+        // 이어서 파고들어가므로 dfs
+        
         for(int i = 0; i < computers.length; i++) {
+            // 다른 컴퓨터와 연결되지 않은 경우 -> 그 컴퓨터 자체로 네트워크 연결
             if(!visited[i]) {
                 answer++;
                 visited[i] = true;
                 dfs(i, computers);
             }
         }
+        
         return answer;
     }
     
-    private void dfs(int num, int[][] computers) {
+    void dfs(int num, int[][] computers) {
         for(int i = 0; i < computers.length; i++) {
             if(num != i && !visited[i] && computers[num][i] == 1) {
                 visited[i] = true;
