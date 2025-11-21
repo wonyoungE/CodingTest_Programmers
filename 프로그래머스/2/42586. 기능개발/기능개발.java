bdd6356c055c3eafa9b69efe77cffe78c4e1995a
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-11-21 12:13:33 +0900

    [level 2] Title: 기능개발, Time: 3.88 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
         
-        // 각 기능마다 걸리는 일 수 계산
+        // 각 기능마다 걸리는 일수 구하기
         for(int i = 0; i < progresses.length; i++) {
-            int days = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            queue.add(days);
+            queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
         }
         
         while(!queue.isEmpty()) {
-            int cnt = 0;
-            int tmp = queue.peek();
+            int count = 0;
+            int maxDay = queue.peek();
             
-            while(!queue.isEmpty() && tmp >= queue.peek()) {
-                cnt++;
+            while(!queue.isEmpty() && maxDay >= queue.peek()) {
                 queue.poll();
+                count++;
             }
             
-            list.add(cnt);
+            list.add(count);
         }
         
-        int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
