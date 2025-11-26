3264d2e710725daa51a17be8f237d483b96a5c97
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-11-26 10:49:09 +0900

    [level 2] Title: 타겟 넘버, Time: 5.50 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,27 @@
 class Solution {
     int answer = 0;
-    int[] numbers;
     int target;
+    int[] numbers;
     
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
+        
         dfs(0, 0);
         
         return answer;
     }
     
-    void dfs(int idx, int sum) {
-        if(idx == numbers.length) {
-            if(sum == target) {
-                answer++;
-            }
+    // 누적할 변수는 어디에?
+    // depth가 몇 번째인지
+    void dfs(int sum, int turn) {
+        if(turn == numbers.length) {
+            if(sum == target) answer++;
             return;
         }
-        dfs(idx + 1, sum + numbers[idx]);
-        dfs(idx + 1, sum - numbers[idx]);
+        // + 
+        dfs(sum + numbers[turn], turn + 1);
+        // -
+        dfs(sum - numbers[turn], turn + 1);
     }
 }
