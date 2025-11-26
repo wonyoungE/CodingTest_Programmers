5e1792f3a93803eae5bfe37e65fb823dbb2766e6
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-11-27 08:24:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.32 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,10 +1,20 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int answer = 0;
-
-        while(a != b) {
-            a = (a+1)/2;
-            b = (b+1)/2;
+        int answer = 1;
+        int x = Math.min(a, b);
+        int y = Math.max(a, b);
+        
+        // 2^1 이상 2^20 이하인 자연수
+        // 시간복잡도?
+        
+        // a + 1 == b이고 a는 홀수, b는 짝수일 경우
+        while(true) {
+            if(x + 1 == y && x % 2 == 1 && y % 2 == 0) {
+                break;
+            }
+            
+            x = (int) Math.ceil(x / 2.0);
+            y = (int) Math.ceil(y / 2.0);
             answer++;
         }
         
