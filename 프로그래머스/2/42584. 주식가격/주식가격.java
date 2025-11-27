13274c626546ca4f998fe18c3abae5bccda279d0
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-11-27 10:24:01 +0900

    [level 2] Title: 주식가격, Time: 83.36 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,11 +4,11 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for(int i = 0; i < prices.length-1; i++) {
+        for(int i = 0; i < prices.length; i++) {
             Stack<Integer> stack = new Stack<>();
-            int current = prices[i];
+            int stock = prices[i];
             for(int j = i; j < prices.length-1; j++) {
-                if(current <= prices[j]) {
+                if(stock <= prices[j]) {
                     stack.push(prices[j]);
                 } else {
                     break;
