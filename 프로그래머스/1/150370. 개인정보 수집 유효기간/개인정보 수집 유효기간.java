08d082b86d8e1e6d50bab1de6c59e7b2cae9400e
WonYoung <129331945+wonyoungE@users.noreply.github.com>
2025-12-01 22:02:11 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.67 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,39 +3,27 @@ import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         
-        int todayTotalDays = getTotalDays(today);
-        
-        Map<String, Integer> period = new HashMap<>();
         for(String term : terms) {
-            String[] split = term.split(" ");
-            period.put(split[0], Integer.parseInt(split[1]));
+            map.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]) * 28);
         }
         
         for(int i = 0; i < privacies.length; i++) {
-            // 유효 기간
-            int valid = period.get(privacies[i].split(" ")[1]);
-            String collectionDate = privacies[i].split(" ")[0];
+            String start = privacies[i].split(" ")[0];
+            String type = privacies[i].split(" ")[1];
             
-            // 수집일을 일 수로 변환
-            int collectionTotalDays = getTotalDays(collectionDate);
-            
-            // 유효기간 만료일 = 수집일 + 유효기간 - 1
-            // 오늘 날짜가 만료일보다 크면 파기
-            if(collectionTotalDays + valid * 28 - 1 < todayTotalDays) {
-                list.add(i + 1);
+            if(toDate(today) > toDate(start) + map.get(type) - 1) {
+                list.add(i+1);
             }
         }
         
         return list.stream().mapToInt(i -> i).toArray();
     }
     
-    private int getTotalDays(String dateStr) {
-        String[] dateSplit = dateStr.split("\\.");
-        int year = Integer.parseInt(dateSplit[0]);
-        int month = Integer.parseInt(dateSplit[1]);
-        int date = Integer.parseInt(dateSplit[2]);
-        
-        return year * 12 * 28 + month * 28 + date;
+    private int toDate(String date) {
+        return Integer.parseInt(date.split("\\.")[0]) * 12 * 28 
+            + Integer.parseInt(date.split("\\.")[1]) * 28 
+            + Integer.parseInt(date.split("\\.")[2]);
     }
 }
